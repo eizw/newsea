efccb0ab04ca0f77422fea98c84c4edf87c93f92
eizw <hamzahamri2005@gmail.com>
2024-05-23 18:35:14 +0800

    Moved domain filter into its own component

--- frontend/src/components/SearchFilter.vue
+++ frontend/src/components/SearchFilter.vue
@@ -10,47 +10,8 @@
             <!-- SOURCES -->
             <SourceFilter :allSources="sources" @sources="setSources" />
 
-            
             <!-- DOMAINS -->
-            <div class="c-filter">
-                <p class="text-l">Domains:</p>
-                <div class="flex flex-col gap-2">
-                    <form class="flex flex-col gap-2"
-                    v-on:submit.prevent="addDomain">
-                        <div class="flex flex-row gap-2">
-                            <input type="search" id="default-search" class="filter-hover filter-textfield"
-                                v-model="newDomain.name" 
-                                placeholder="Add Domain..."/>
-                            <button type="submit" class="filter-add-btn filter-hover">
-                                <i class="fa-solid fa-plus text-sm px-2 text-newsea-primary" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                        
-                        <div class="flex flex-1 items-center">
-                            <input type="checkbox" id="filter-include"
-                            v-model="newDomain.exclude" class="w-4 h-4 filter-hover
-                            text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
-                            <label for="filter-include" class="ms-2 text-sm font-medium text-black">exclude</label>
-                        </div>
-                    </form>
-                    
-                    <form class="flex flex-row"
-                    v-on:submit.prevent
-                    v-for="i in filters.domains.length"
-                    key="i"
-                    >
-                        <input type="search" id="default-search" class="filter-hover filter-textfield"
-                            v-model="filters.domains[i-1].name"
-                            :class="[
-                                filters.domains[i-1].exclude ? 'filter-exclude' : 'filter-include',
-                            ]"
-                            :placeholder="'Domain ' + (i)" required/>
-                        <button type="submit" v-on:click="removeDomain(i-1)">
-                            <i class="fa-solid fa-trash text-sm pl-3 text-red-500" aria-hidden="true"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
+            <DomainFilter @domains="setDomains"/>
 
             <!-- DATE -->
             <div class="c-filter">
@@ -70,6 +31,7 @@
     import LangFilter from '@/components/filters/LangFilter.vue';
     import SearchInFilter from '@/components/filters/SearchInFilter.vue';
     import SourceFilter from '@/components/filters/SourceFilter.vue';
+    import DomainFilter from '@/components/filters/DomainFilter.vue';
 
     const store = useStore();
 
@@ -84,7 +46,7 @@
         language: '',
         searchIn: [] as string[],
         sources: [] as string[],
-        domains: [] as any[],
+        domains: [] as string[],
         excludeDomains: [] as string[],
     })
 
@@ -105,31 +67,11 @@
     }
     const setSources = (val: string[]) => {
         filters.value.sources = val;
-        console.log(filters.value)
+    }
+    const setDomains = (val: string[]) => {
+        filters.value.domains = val;
     }
     
 
-    // DOMAINS = name
-    const newDomain = ref({
-        exclude: false,
-        name: ''
-    })
-    const addDomain = () => {
-        if (newDomain.value.name == '') {
-            return
-        }
-        if (filters.value.domains.indexOf(newDomain.value) > -1) {
-            return
-        }
-        console.log(newDomain.value)
-        filters.value.domains.push(newDomain.value)
-        filters.value.domains.splice(filters.value.domains.length+1)
-        newDomain.value = {
-            exclude: false,
-            name: ''
-        }
-    }
-    const removeDomain = (i: number) => {
-        filters.value.domains.splice(i, 1)
-    }
+    
 </script>
---
+++ frontend/src/components/filters/DomainFilter.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="c-filter">
+        <p class="text-l">Domains:</p>
+        <div class="flex flex-col gap-2">
+            <form class="flex flex-col gap-2"
+            v-on:submit.prevent="addDomain">
+                <div class="flex flex-row gap-2">
+                    <input type="search" id="default-search" class="filter-hover filter-textfield"
+                        v-model="newDomain.name" 
+                        placeholder="Add Domain..."/>
+                    <button type="submit" class="filter-add-btn filter-hover">
+                        <i class="fa-solid fa-plus text-sm px-2 text-newsea-primary" aria-hidden="true"></i>
+                    </button>
+                </div>
+                
+                <div class="flex flex-1 items-center">
+                    <input type="checkbox" id="filter-include"
+                    v-model="newDomain.exclude" class="w-4 h-4 filter-hover
+                    text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
+                    <label for="filter-include" class="ms-2 text-sm font-medium text-black">exclude</label>
+                </div>
+            </form>
+            
+            <form class="flex flex-row"
+            v-on:submit.prevent
+            v-for="i in domains.length"
+            key="i"
+            >
+                <input type="search" id="default-search" class="filter-hover filter-textfield"
+                    v-model="domains[i-1].name"
+                    :class="[
+                        domains[i-1].exclude ? 'filter-exclude' : 'filter-include',
+                    ]"
+                    :placeholder="'Domain ' + (i)" required/>
+                <button type="submit" v-on:click="removeDomain(i-1)">
+                    <i class="fa-solid fa-trash text-sm pl-3 text-red-500" aria-hidden="true"></i>
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref, watch } from 'vue';
+
+    const emit = defineEmits(['domains'])
+
+    const domains = ref([] as any[])
+
+    watch(domains.value, (val) => {
+        let temp = [] as string[]
+        domains.value.map((domain: any) => {
+            temp.push(domain.name)
+        })
+        emit('domains', temp)
+    })
+
+    // DOMAINS = name
+    const newDomain = ref({
+        exclude: false,
+        name: ''
+    })
+    const addDomain = () => {
+        if (newDomain.value.name == '') {
+            return
+        }
+        if (domains.value.indexOf(newDomain.value) > -1) {
+            return
+        }
+        console.log(newDomain.value)
+        domains.value.push(newDomain.value)
+        domains.value.splice(domains.value.length+1)
+        newDomain.value = {
+            exclude: false,
+            name: ''
+        }
+    }
+    const removeDomain = (i: number) => {
+        domains.value.splice(i, 1)
+    }
+</script>
